284453cf1956c95339e37c6a67f84288ae7ec9f4
unknown <u1356770@CW504-03.AD.HUD.AC.UK>
2015-12-03 10:16:14 +0000

    p21 add player object

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public TileData CommanderPosition;
+	public PointsSystem Points;
+	public PlayerType Type;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Empire - The Last Battle/Assets/Unity/Scripts/ScriptableObjects/PlayerTypeData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public GameObject[] Players;
+}
+
+public enum PlayerType
+{
+	Battlebeard,
+	Stormshaper,
+	Neurtral
+}
