53a3346b9d50780211d8bbfd6716d22e7d70979a
Yoshimiii <martino1995@blueyonder.co.uk>
2015-12-15 11:13:43 +0000

    Alliance card functionality

--- Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/CardSystem.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/CardSystem.cs
@@ -4,89 +4,86 @@ using UnityEngine;
 
 public enum Cards
 {
-	Healing_Card_1,
-	Healing_Card_2,
-	Resource_Card_100,
-	Resource_Card_200,
-	Resource_Card_300,
-	Resource_Card_400,
-	Resource_Card_500,
-	Battle_Card_1,
-	Tactic_Card_1,
-	Tactic_Card_2,
-	Tactic_Card_3,
-	Alliance_Card_1,
-	Scout_Card_1,
-	Priority_Card_1,
-	Upgrade_Card_1,
-	Upgrade_Card_2
+	Healing_Card_1 = 1,
+	Healing_Card_2 = 2,
+	Resource_Card_100 = 100,
+	Resource_Card_200 = 200,
+	Resource_Card_300 = 300,
+	Resource_Card_400 = 400,
+	Resource_Card_500 = 500,
+	Battle_Card_1 = 1,
+	Tactic_Card_1 = 1,
+	Tactic_Card_2 = 2,
+	Tactic_Card_3 = 3,
+	Alliance_Card_1 = 1,
+	Scout_Card_1 = 1,
+	Priority_Card_1 = 1,
+	Upgrade_Card_1 = 1,
+	Upgrade_Card_2 = 2
 }
 
 public class CardSystem : MonoBehaviour
 {
 	public Dictionary<int, GameObject> cardsLinker;
 
-	public delegate void CardEffectCallback(Cards card);
-	public event CardEffectCallback OnEffectApplied = delegate { };
+	public delegate void CardCallback(int amount, Cards card, Player player);
+	public event CardCallback OnEffectApplied = delegate { };
+	public event CardCallback OnHealingCardUsed = delegate { };
 
 	public void ApplyEffect(Cards card, Player player) {
 		switch (card) {
 			case Cards.Healing_Card_1:
-				UseHealingCard(1, player);
-				break;
 			case Cards.Healing_Card_2:
-				UseHealingCard(2, player);
+				RegisterCardHeal(card, player);
 				break;
 			case Cards.Resource_Card_100:
-				UseResourceCard(100, player);
-				break;
 			case Cards.Resource_Card_200:
-				UseResourceCard(200, player);
-				break;
 			case Cards.Resource_Card_300:
-				UseResourceCard(300, player);
-				break;
 			case Cards.Resource_Card_400:
-				UseResourceCard(400, player);
-				break;
 			case Cards.Resource_Card_500:
-				UseResourceCard(500, player);
+				UseResourceCard((int)card, player);
 				break;
 			case Cards.Battle_Card_1:
-				UseResourceCard(1, player);
+				UseBattleCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			case Cards.Tactic_Card_1:
-				UseTacticCard(1, player);
-				break;
 			case Cards.Tactic_Card_2:
-				UseTacticCard(2, player);
-				break;
 			case Cards.Tactic_Card_3:
-				UseTacticCard(3, player);
+				UseTacticCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			case Cards.Alliance_Card_1:
-				UseAllianceCard(1, player);
+				UseAllianceCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			case Cards.Scout_Card_1:
-				UseScoutCard(1, player);
+				UseScoutCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			case Cards.Priority_Card_1:
-				UsePriorityCard(1, player);
+				UsePriorityCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			case Cards.Upgrade_Card_1:
-				UseUpgradeCard(1, player);
-				break;
 			case Cards.Upgrade_Card_2:
-				UseUpgradeCard(2, player);
+				UseUpgradeCard((int)card, player);
+				OnEffectApplied(card, player);
 				break;
 			default:
 				break;
 		}
-		OnEffectApplied(card);
 	}
 
-	private void UseHealingCard(int amount, Player player) {
-		throw new NotImplementedException();
+	private void RegisterCardHeal(Cards card, Player player) {
+		OnHealingCardUsed ((int)card, card, player);
+	}
+
+	public void UseHealingCard(Player player, List<Unit> unitsToHeal) {
+		foreach (var unit in unitsToHeal) {
+			unit.Heal();
+		}
+
 	}
 
 	private void UseResourceCard(int amount, Player player) {
