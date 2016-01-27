a1db817553d978cac3c83a1e4dcd3866cbeaba52
Martin <martino1995@blueyonder.co.uk>
2016-01-27 13:08:19 +0000

    Added rotation to player

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/CommanderAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommanderAnimation : MonoBehaviour
+{
+	public CommanderUI CommanderUI;
+
+	public float Angle;
+	public float Period;
+
+	float _time = 0f;
+
+	void Start()
+	{
+		CommanderUI = GetComponentInChildren<CommanderUI>();
+		CommanderUI.OnDraggingCommander += Animate;
+		CommanderUI.OnCommanderGrounded += Reset;
+	}
+
+	void Animate()
+	{
+		_time = _time + Time.deltaTime;
+		float phase = Mathf.Sin(_time / Period);
+		transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, phase * Angle));
+	}
+
+	void Reset()
+	{
+		transform.rotation.Set(0.0f, 0.0f, 0.0f, 0.0f);
+	}
+}
